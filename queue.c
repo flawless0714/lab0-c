9280ab269836ba02642752b27d09e5ba8800ce4e
flawless0714 <flawless0714@gmail.com>
2018-10-19 04:27:04 +0800

    First complete of queue.c and .h
    A earlier version was intended to be published, but due
    to the code is buggy, it didn't even pass the check
    (memory leak error)

--- queue.c
+++ queue.c
@@ -28,11 +28,12 @@ queue_t *q_new()
     /* What if malloc returned NULL? */
     /* the following is going to solve the problem asked above */
     if (q == NULL)
-        return NULL; /* we return NULL directly instead of reference q is for
-                        efficiency */
+        return NULL;
     /* note that we need to initialize more member if we add more member later
      */
     q->head = NULL;
+    q->tail = NULL;
+    q->eleCount = 0;
     return q;
 }
 
@@ -41,6 +42,16 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
+    if (q == NULL)
+        return; /* nothing to free */
+    list_ele_t *next = q->head, *tmp;
+
+    while (next) {
+        tmp = next->next;
+        free(next->value);
+        free(next);
+        next = tmp;
+    }
     free(q);
 }
 
@@ -53,13 +64,30 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (q == NULL)
+        return false;
     list_ele_t *newh;
     /* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+
+    newh->value = malloc((strlen(s) + 1) * sizeof(char));
+    if (newh->value == NULL) {
+        free(newh);
+        return false;
+    }
+    strcpy(newh->value, s);
+    newh->value[strlen(s)] = '\0'; /* last element of the string */
+
     newh->next = q->head;
     q->head = newh;
+    if (q->tail == NULL)
+        q->tail = newh;
+
+    q->eleCount++;
     return true;
 }
 
@@ -75,7 +103,30 @@ bool q_insert_tail(queue_t *q, char *s)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    if (q == NULL)
+        return false;
+    list_ele_t *newt;
+    newt = malloc(sizeof(list_ele_t));
+    if (newt == NULL)
+        return false;
+    newt->next = NULL;
+    newt->value = malloc((strlen(s) + 1) * sizeof(char));
+    if (newt->value == NULL) {
+        free(newt);
+        return false;
+    }
+    strcpy(newt->value, s);
+    newt->value[strlen(s)] = '\0';
+
+    /* update the node */
+    if (q->head == NULL)
+        q->head = newt;
+    else
+        q->tail->next = newt;
+    q->tail = newt;
+
+    q->eleCount++;
+    return true;
 }
 
 /*
@@ -89,7 +140,30 @@ bool q_insert_tail(queue_t *q, char *s)
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* You need to fix up this code. */
-    q->head = q->head->next;
+    /* Q: difference beteween NULL and empty A: NULL indicate that q has no
+       address no address stored, empty indicate that queue has no element
+       stored */
+    if (q == NULL)
+        return false;
+    if (q->head == NULL)
+        return false;
+    /* Q: why struct ELE has size 16 bytes, isn't it just a pointer same
+       as list_ele_t? */
+    /* ! not sure if bufsize - 1 included null terminator, here we counted */
+    list_ele_t *tmp;
+    if (sp != NULL) {
+        if ((strlen(q->head->value) + 1) < bufsize) {
+            strcpy(sp, q->head->value);
+            sp[strlen(sp)] = '\0';
+        }
+    }
+
+    tmp = q->head->next;
+    free(q->head->value);
+    free(q->head);
+    q->head = tmp;
+
+    q->eleCount--;
     return true;
 }
 
@@ -101,6 +175,8 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
+    if (q)
+        return q->eleCount;
     return 0;
 }
 
@@ -114,4 +190,33 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     /* You need to write the code for this function */
+    if (q == NULL)
+        return;
+    if (q->head == NULL)
+        return;
+    list_ele_t *previous, *preceding, *tmp;
+
+    if (q->head->next) { /* positive reverse */
+        preceding = q->head->next;
+        previous = q->head;
+
+        while (preceding) {
+            tmp = preceding->next;
+            preceding->next = previous;
+            previous = preceding;
+            preceding = tmp;
+        }
+        q->head->next = NULL;
+    } else if (q->tail->next) { /* negative reverse */
+        preceding = q->tail->next;
+        previous = q->tail;
+
+        while (preceding) {
+            tmp = preceding->next;
+            preceding->next = previous;
+            previous = preceding;
+            preceding = tmp;
+        }
+        q->tail->next = NULL;
+    }
 }
